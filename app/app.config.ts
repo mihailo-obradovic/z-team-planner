ea33b2cbd6e24ccf6effeba93b3ba0d579077921
Mihailo Obradović <mihailo.obradovic.95@gmail.com>
2026-02-04 16:43:47 +0100

    Set up NuxtUI config

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,8 +1,20 @@
+// ! Don't use aliases because .nuxt/ is not available during app.config.ts load in SSR mode
+import button from './config/nuxt-ui/button';
+
 export default defineAppConfig({
+  // https://ui.nuxt.com/getting-started/theme#design-system
+  icon: { mode: 'svg', cssLayer: 'base' },
   ui: {
     colors: {
       primary: 'green',
       neutral: 'slate'
-    }
+      // secondary: 'secondary',
+      // info: 'info',
+      // success: 'success',
+      // warning: 'warning',
+      // error: 'error',
+      // accent: 'accent'
+    },
+    button
   }
-})
+});
---
+++ app/types/nuxt-ui.d.ts
@@ -0,0 +1,4 @@
+import type * as theme from '#build/ui';
+import type { TVConfig } from '#ui/types';
+
+export type ButtonConfig = TVConfig<typeof theme>['button'];
