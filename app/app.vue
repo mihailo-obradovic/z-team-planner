245b5c2ae09d921487067ef8fbcee5dcd8d175c3
Mihailo Obradović <mihailo.obradovic.95@gmail.com>
2026-02-04 17:06:55 +0100

    Add Prettier, run scripts

--- app/app.vue
+++ app/app.vue
@@ -1,18 +1,15 @@
 <script setup>
 useHead({
-  meta: [
-    { name: 'viewport', content: 'width=device-width, initial-scale=1' }
-  ],
-  link: [
-    { rel: 'icon', href: '/favicon.ico' }
-  ],
+  meta: [{ name: 'viewport', content: 'width=device-width, initial-scale=1' }],
+  link: [{ rel: 'icon', href: '/favicon.ico' }],
   htmlAttrs: {
     lang: 'en'
   }
-})
+});
 
-const title = 'Nuxt Starter Template'
-const description = 'A production-ready starter template powered by Nuxt UI. Build beautiful, accessible, and performant applications in minutes, not hours.'
+const title = 'Nuxt Starter Template';
+const description
+  = 'A production-ready starter template powered by Nuxt UI. Build beautiful, accessible, and performant applications in minutes, not hours.';
 
 useSeoMeta({
   title,
@@ -22,7 +19,7 @@ useSeoMeta({
   ogImage: 'https://ui.nuxt.com/assets/templates/nuxt/starter-light.png',
   twitterImage: 'https://ui.nuxt.com/assets/templates/nuxt/starter-light.png',
   twitterCard: 'summary_large_image'
-})
+});
 </script>
 
 <template>
